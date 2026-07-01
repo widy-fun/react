f2ac5c6392fd489dea64c758b1c8fe1e5e67ed11
ik1s3v <ik1s3v@gmail.com>
2026-07-01 13:09:02 +0300

    feat: add AppTheme type

--- src/theme/muiTheme.ts
+++ src/theme/muiTheme.ts
@@ -1,5 +1,7 @@
-import type { ThemeOptions } from "@mui/material";
+import type { Theme, ThemeOptions } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { darkTheme } from "@widy/sdk";
+
+export type AppTheme = typeof darkTheme & Theme;
 
 export const dark = createTheme(darkTheme as ThemeOptions);
